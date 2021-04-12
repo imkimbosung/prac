2742a215ac8965b838b56f42512ee84c1f398d04
DESKTOP-MLKK7KD\GENEXON <wonone95@naver.com>
2021-04-12 13:30:41 +0900

    Recording study Algorithm

---
+++ CodingTest/cwh/week_01/Recursion_v02.java
@@ -0,0 +1,31 @@
+package week_01;
+
+public class Recursion_v02 {
+
+	public static void main(String[] args) {
+		/*
+         * Recursion Algorithm version 02 
+         *  
+         * Recursion function Method Type is int.
+         * main Method calling Recursion function and return int Type.
+         * 
+         * # 알고리즘 강의 영상 본 후 작성한 코드
+         * 
+         * Finaly Parameter num is return 0 and sysout is print "Total Odd Number" 
+         * 
+         */
+		int result = Recursion(100);
+        System.out.println("Total Odd Number = " + result);
+
+	}
+	
+	public static int Recursion(int num){
+        if( num == 0){
+            return 0;
+        }else if( num%2==1){
+            return num + Recursion(num-1);
+        }else{
+            return 0 + Recursion(num-1);
+        }
+    }
+}
